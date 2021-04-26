ae2263b0eff7e7667bfc01920f9f57e9d27f0d63
saimir djoumoi <saimir.djoumoi@gmail.com>
2021-04-26 13:12:50 +0200

    test routes home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Footer from '../Components/Footer'
+import Header from '../Components/Header'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                
+                <Header/>
+                Acceuil
+                <Footer/>
             </div>
         )
     }
