5b51f766dbc8537703c0902134e520d6acadfbc2
saimir djoumoi <saimir.djoumoi@gmail.com>
2021-04-26 13:11:07 +0200

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './scss/App.scss';
+import Header from './Components/Header'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+      <Switch>
+        <Route path="/">
+
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/role">
+
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/actions">
+
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/pour-le-canton">
+
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
