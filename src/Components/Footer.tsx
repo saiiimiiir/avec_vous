6b677e349888292f8965453a79a2226b7b6fe104
saimir.djoumoi <saimir.djoumoi@gmail.com>
2021-04-27 15:34:23 +0200

    footer fait

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import '../scss/Footer.scss'
+import logoLong from '../asset/logo_long.png'
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
+            <div id="footer">
+                <div className="logoFooter">
+                    <img src={logoLong} alt="" />
+                    <span className="bluetext">© 2021 majorité départementale loiret avec vous</span>
+                </div>
+                <div className="contact">
+                    <i className="fas fa-envelope"></i> <span className="bluetext">Nous contacter</span>
+                </div>
+                <div className="mention">
+                    <span className="bluetext">mentions légales</span>
+                </div>
             </div>
         )
     }
